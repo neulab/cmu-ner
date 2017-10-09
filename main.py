cfb4e1bb2de7b80b0c621cf9d1263f1f99ceb27d
violet-zct <chunting.violet.zhou@gmail.com>
2017-10-09 10:56:36 -0400

    oops

---
+++ main.py
@@ -0,0 +1,88 @@
+__author__ = 'chuntingzhou'
+import argparse
+from dataloaders.data_loader import *
+from models.model_builder import *
+import time
+
+
+def evaluate(data_loader, path, model):
+    sents, char_sents, tgt_tags, discrete_features = data_loader.get_data_set(path)
+
+    tot_acc = 0.0
+    for sent, char_sent, tgt_tag, discrete_feature in zip(sents, char_sents, tgt_tags, discrete_features):
+        sent, char_sent, discrete_feature = [sent], [char_sent], [discrete_feature]
+        best_score, best_path = model.eval(sent, char_sent, discrete_feature)
+
+        assert len(best_path) == len(tgt_tag)
+        acc = model.crf_decoder.cal_accuracy(best_path, tgt_tags)
+        tot_acc += acc
+    return tot_acc / len(sents)
+
+
+def main(args):
+    ner_data_loader = NER_DataLoader(args)
+
+    sents, char_sents, tgt_tags, discrete_features = ner_data_loader.get_data_set(args.train_path)
+
+    epoch = bad_counter = updates = cum_acc = tot_example = 0
+    patience = 20
+
+    display_freq = 10
+    valid_freq = args.valid_freq
+    batch_size = args.batch_size
+
+    model = vanilla_NER_CRF_model(args, ner_data_loader)
+    trainer = dy.MomentumSGDTrainer(model.model, 0.015, 0.9)
+
+    valid_history = []
+    while epoch <= args.tot_epochs:
+        epoch += 1
+        for b_sents, b_char_sents, b_ner_tags, b_feats in make_bucket_batches(
+                zip(sents, char_sents, tgt_tags, discrete_features), batch_size):
+            dy.renew_cg()
+            loss = model.cal_loss(b_sents, b_char_sents, b_ner_tags, b_feats)
+            loss_val = loss.value()
+
+            updates += 1
+            loss.backward()
+            trainer.update()
+
+            if display_freq % updates == 0:
+                print("Epoch = %d, Updates = %d, CRF Loss=%f." % (epoch, updates, loss_val))
+            if updates % valid_freq == 0:
+                pass
+
+
+# add task specific trainer and args
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dynet-mem", default=1000, type=int)
+    parser.add_argument("--dynet-seed", type=int)
+
+    parser.add_argument("--train_path", default="../datasets/english/eng.train.bio.conll", type=str)
+    parser.add_argument("--dev_path", default="../datasets/english/eng.dev.bio.conll", type=str)
+    parser.add_argument("--test_path", default="../datasets/english/eng.test.bio.conll", type=str)
+
+    parser.add_argument("--tag_emb_dim", default=50, type=int)
+    parser.add_argument("--pos_emb_dim", default=50, type=int)
+    parser.add_argument("--char_emb_dim", default=30, type=int)
+    parser.add_argument("--word_emb_dim", default=100, type=int)
+    parser.add_argument("--cnn_filter_size", default=30, type=int)
+    parser.add_argument("--cnn_win_size", default=3, type=int)
+    parser.add_argument("--rnn_type", default="lstm", type=str)
+    parser.add_argument("--hidden_dim", default=200, type=int)
+    parser.add_argument("--layer", default=1, type=int)
+
+    parser.add_argument("--dropout_rate", default=0.5, type=float)
+    parser.add_argument("--valid_freq", default=500, type=int)
+    parser.add_argument("--tot_epochs", default=100)
+    parser.add_argument("--batch_size", default=10)
+
+    parser.add_argument("--tagging_scheme", default="bio", choices=["bio", "bioes"], type=str)
+    parser.add_argument("--data_aug", default=False, action="store_true")
+    parser.add_argument("--pretrain_emb_path", type=str, default=None)
+    parser.add_argument("--pretrain_finetune", default="False", action="store_true")
+    parser.add_argument("--use_discrete_features", default=False, action="store_true")
+    parser.add_argument("--feature_dim", type=int, default=50)
+    args = parser.parse_args()
+    main(args)
---
+++ models/decoders.py
@@ -0,0 +1,143 @@
+__author__ = 'chuntingzhou'
+from utils import *
+
+
+class Decoder():
+    def __init__(self):
+        # type: () -> object
+        pass
+
+    def decode_loss(self):
+        raise NotImplementedError
+
+    def decoding(self):
+        raise NotImplementedError
+
+
+class chain_CRF_decoder(Decoder):
+    ''' For NER and POS Tagging. '''
+
+    def __init__(self, model, src_output_dim, tag_emb_dim, tag_size):
+        Decoder.__init__(self)
+        self.model = model
+
+        self.start_id = tag_size
+        self.end_id = tag_size + 1
+        self.tag_size = tag_size + 2
+        tag_size = tag_size + 2
+        # optional: transform the hidden space of src encodings into the tag embedding space
+        self.W_src2tag_readout = model.add_parameters((tag_emb_dim, src_output_dim))
+        self.b_src2tag_readout = model.add_parameters((tag_emb_dim))
+        self.b_src2tag_readout.zero()
+
+        self.W_scores_readout2tag = model.add_parameters((tag_size, tag_emb_dim))
+        self.b_scores_readout2tag = model.add_parameters((tag_size))
+        self.b_scores_readout2tag.zero()
+
+        # (to, from), trans[i] is the transition score to i
+        init_transition_matrix = np.random.randn(tag_size, tag_size)
+        init_transition_matrix[self.start_id, :] = -10000.0
+        init_transition_matrix[:, self.end_id] = -10000.0
+        self.transition_matrix = model.add_lookup_parameters((tag_size, tag_size),
+                                                             init=dy.NumpyInitializer(init_transition_matrix))
+
+    def forward_alg(self, tag_scores, batch_size):
+        ''' Forward DP for CRF.
+        tag_scores (list of batched dy.Tensor): (tag_size, batchsize)
+        '''
+        transition_score = dy.parameter(self.transition_matrix)
+        transpose_transition_score = dy.transpose(transition_score)  # (from, to)
+        # alpha(t', s) = the score of sequence from t=0 to t=t' in log space
+        np_init_alphas = -10000.0 * np.ones((self.tag_size, batch_size))
+        np_init_alphas[self.start_id, :] = 0.0
+        alpha_tm1 = dy.inputTensor(np_init_alphas, batched=True)
+
+        for tag_score in tag_scores:
+            # extend for each transit <to>
+            alpha_tm1 = dy.concatenate_cols([alpha_tm1] * self.tag_size)  # (from, to, batch_size)
+            # each column i of tag_score will be the repeated emission score to tag i
+            tag_score = dy.transpose(dy.concatenate_cols([tag_score] * self.tag_size))
+            alpha_t = alpha_tm1 + transpose_transition_score + tag_score
+            alpha_tm1 = log_sum_exp_dim_0(alpha_t)  # (tag_size, batch_size)
+
+        terminal_alpha = log_sum_exp_dim_0(alpha_tm1 + transition_score[self.end_id])  # (1, batch_size)
+        return terminal_alpha
+
+    def score_one_sequence(self, tag_scores, tags, batch_size):
+        ''' tags: list of tag ids at each time step '''
+        tags = [self.start_id] * batch_size + tags  # len(tag_scores) = len(tags) - 1
+        score = dy.inputTensor(np.zeros(batch_size), batched=True)
+        for i in range(len(tags) - 1):
+            score = score + dy.pick_batch(dy.lookup_batch(self.transition_matrix, tags[i + 1]), tags[i]) \
+                    + dy.pick_batch(tag_scores[i], tags[i + 1])
+        score += dy.pick_batch(dy.lookup_batch(self.transition_matrix, [self.end_id] * batch_size), tags[-1])
+        return score
+
+    def decode_loss(self, src_encodings, tgt_tags):
+        # This is the batched version which requires bucketed batch input with the same length.
+        '''
+        The length of src_encodings and tgt_tags are time_steps.
+        src_encodings: list of dynet.Tensor (src_output_dim, batch_size)
+        tgt_tags: list of tag ids [(1, batch_size)]
+        return: average of negative log likelihood
+        '''
+        # TODO: transpose tgt tags first
+        batch_size = len(tgt_tags[0])
+        tgt_tags, tgt_mask = transpose_input(tgt_tags, 0)
+        W_src2tag_readout = dy.parameter(self.W_src2tag_readout)
+        b_src2tag_readout = dy.parameter(self.b_src2tag_readout)
+        W_score_tag = dy.parameter(self.W_scores_readout2tag)
+        b_score_tag = dy.parameter(self.b_scores_readout2tag)
+
+        tag_embs = [dy.tanh(dy.affine_transform([b_src2tag_readout, W_src2tag_readout, src_encoding])) for src_encoding
+                    in src_encodings]
+        tag_scores = [dy.affine_transform([b_score_tag, W_score_tag, tag_emb]) for tag_emb in tag_embs]
+
+        # scores over all paths, all scores are in log-space
+        forward_scores = self.forward_alg(tag_scores, batch_size)
+        gold_score = self.score_one_sequence(tag_scores, tgt_tags, batch_size)
+        loss = dy.sum_batches(gold_score - forward_scores) / batch_size
+        return loss
+
+    def decoding(self, src_encodings):
+        ''' Viterbi decoding for a single sequence. '''
+        W_src2tag_readout = dy.parameter(self.W_src2tag_readout)
+        b_src2tag_readout = dy.parameter(self.b_src2tag_readout)
+        W_score_tag = dy.parameter(self.W_scores_readout2tag)
+        b_score_tag = dy.parameter(self.b_scores_readout2tag)
+
+        tag_embs = [dy.tanh(dy.affine_transform([b_src2tag_readout, W_src2tag_readout, src_encoding]))
+                    for src_encoding in src_encodings]
+        tag_scores = [dy.affine_transform([b_score_tag, W_score_tag, tag_emb]) for tag_emb in tag_embs]
+
+        back_trace_tags = []
+        np_init_alpha = np.ones(self.tag_size) * -10000.0
+        np_init_alpha[self.start_id] = 0.0
+        max_tm1 = dy.inputTensor(np_init_alpha)
+        transition_score = dy.parameter(self.transition_matrix)  # (to, from)
+        transpose_transition_score = dy.transpose(transition_score)  # (from, to)
+
+        for tag_score in tag_scores:
+            max_tm1 = dy.concatenate_cols([max_tm1] * self.tag_size)
+            max_t = max_tm1 + transpose_transition_score
+            eval_score = max_t.npvalue()
+            best_tag = np.argmax(eval_score, axis=0)
+            back_trace_tags.append(best_tag)
+            max_tm1 = dy.inputTensor(eval_score[best_tag, range(self.tag_size)]) + tag_score
+
+        terminal_max_T = max_tm1 + self.transition_matrix[self.end_id]
+        eval_terminal = terminal_max_T.npvalue()
+        best_tag = np.argmax(eval_terminal, axis=0)
+        best_path_score = eval_terminal[best_tag]
+
+        best_path = [best_tag]
+        for btpoint in reversed(back_trace_tags):
+            best_tag = btpoint[best_tag]
+            best_path.append(best_tag)
+        start = best_path.pop()
+        assert start == self.start_id
+        best_path.reverse()
+        return best_path_score, best_path
+
+    def cal_accuracy(self, pred_path, true_path):
+        return np.sum(np.equal(pred_path, true_path))/len(pred_path)
