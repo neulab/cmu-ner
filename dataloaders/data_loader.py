2b842e50deb88e37b262e72d147c711b73febc53
violet-zct <chunting.violet.zhou@gmail.com>
2017-11-01 17:59:58 -0400

    fix the bug: no <unk> in the vocab

--- dataloaders/data_loader.py
+++ dataloaders/data_loader.py
@@ -26,10 +26,10 @@ class NER_DataLoader():
             print("Done!")
         else:
             self.tag_to_id, self.word_to_id, self.char_to_id = self.read_file()
-            self.word_to_id['<unk>'] = len(self.word_to_id)
             self.word_to_id['<eos>'] = 0
-            self.char_to_id['<unk>'] = len(self.char_to_id)
             self.char_to_id['<pad>'] = 0
+            self.word_to_id['<unk>'] = len(self.word_to_id)
+            self.char_to_id['<unk>'] = len(self.char_to_id)
 
             pkl_dump(self.tag_to_id, self.tag_vocab_path)
             pkl_dump(self.char_to_id, self.char_vocab_path)
@@ -50,6 +50,8 @@ class NER_DataLoader():
         self.word_vocab_size = len(self.id_to_word)
         self.char_vocab_size = len(self.id_to_char)
 
+        print self.word_to_id
+        print self.id_to_word
         print("NER tag num=%d, Word vocab size=%d, Char Vocab size=%d" % (self.ner_vocab_size, self.word_vocab_size, self.char_vocab_size))
 
     @staticmethod
