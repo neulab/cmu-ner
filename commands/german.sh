a54b7110c2812ad5c9a7939c337c3aa83398ca64
Chunting Zhou <chuntinz@lor.lti.cs.cmu.edu>
2017-11-08 15:02:20 -0500

    11

--- commands/german.sh
+++ commands/german.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+MODEL_NAME=$1
 python ../main.py \
     --dynet-seed 5783287 \
     --word_emb_dim 64 \
@@ -6,16 +7,15 @@ python ../main.py \
     --train_path ../datasets/german/deu.train.utf8.conll \
     --dev_path ../datasets/german/deu.testa.utf8.conll \
     --test_path ../datasets/german/deu.testb.utf8.conll \
-    --pretrain_emb_path ../datasets/ger_emb.txt \
+    --pretrain_emb_path ../datasets/embs/sskip/ger_emb.txt \
     --emb_dropout_rate 0.0 \
     --output_dropout_rate 0.5 \
     --init_lr 0.01 \
     --model_arc char_birnn \
-    --output_dropout_rate 0.0 \
     --tag_emb_dim 100 \
     --hidden_dim 100 \
     --char_emb_dim 30\
     --char_hidden_dim 25 \
     --lang german \
     --replace_unk_rate 0.5 \
-    --valid_freq 1300
+    --valid_freq 1300 2>&1 | tee ${MODEL_NAME}
