a5814963e3b0a8b5b6daf0e0574c685d0ef674ec
violet-zct <chunting.violet.zhou@gmail.com>
2017-11-14 14:08:21 -0500

    use unicode when evaluation

--- utils/Convert_Output_Darpa.py
+++ utils/Convert_Output_Darpa.py
@@ -7,7 +7,7 @@ def run_program(input, output, setEconll):
     reload(sys)	
     sys.setdefaultencoding('utf-8')
     if input is not None and setEconll is not None:
-        with codecs.open(input, 'r',encoding='utf-8', errors='ignore') as input_file:
+        with codecs.open(input, 'r', encoding='utf-8', errors='ignore') as input_file:
             lines = input_file.readlines()
         tags = []
         for i, line in enumerate(lines):
@@ -24,6 +24,39 @@ def run_program(input, output, setEconll):
             lines = input_file.readlines()
         assert len(output_lines) == len(lines)
         with codecs.open(output,'w',encoding='utf-8') as output_file:
+            ctr = -1
+            for line in lines:
+                if len(line) > 2:
+                    ctr += 1
+                    line_split = line.strip().split()
+                    assert len(line_split) == 10
+                    print '\t'.join(line_split) + '\t' + tags[ctr]
+                    output_file.write('\t'.join(line_split) + '\t' + tags[ctr] +"\n")
+                else:
+                    print ""
+                    output_file.write("\n")
+            assert ctr + 1 == len(tags)
+
+
+def run_program_2(input, output, setEconll):
+    if input is not None and setEconll is not None:
+        with codecs.open(input, 'r') as input_file:
+            lines = input_file.readlines()
+        tags = []
+        for i, line in enumerate(lines):
+            if len(line) >= 2:
+                line_split = line.strip().split()
+                sys.stderr.write('line: ' + line.strip() + '\n')
+                sys.stderr.flush()
+                assert len(line_split) == 4
+                tags.append(line_split[-1])
+
+        output_lines = lines
+
+        with codecs.open(setEconll, 'r') as input_file:
+            lines = input_file.readlines()
+        assert len(output_lines) == len(lines)
+        with codecs.open(output, 'w') as output_file:
             ctr = -1
             for line in lines:
                 if len(line) > 2:
