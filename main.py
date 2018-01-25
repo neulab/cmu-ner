31f75d92dbe5c3418f8cba89d9605f283884d99d
Aditi <aditiasac@gmail.com>
2018-01-25 10:05:41 -0500

    adding eval

--- main.py
+++ main.py
@@ -171,6 +171,30 @@ def replace_singletons(data_loader, sents, replace_rate):
         new_batch_sents.append(new_sent)
     return new_batch_sents
 
+def evaluate_test(ner_data_loader, path, model_name):
+
+    if args.model_arc == "char_cnn":
+        print "Using Char CNN model!"
+	model = vanilla_NER_CRF_model(args, ner_data_loader)
+    elif args.model_arc == "char_birnn":
+	print "Using Char Birnn model!"
+	model = BiRNN_CRF_model(args, ner_data_loader)
+    elif args.model_arc == "char_birnn_cnn":
+	print "Using Char Birnn-CNN model!"
+	model = CNN_BiRNN_CRF_model(args, ner_data_loader)
+    elif args.model_arc == "sep":
+	print "Using seperate encoders for embedding and features (cnn and birnn char)!"
+	model = Sep_Encoder_CRF_model(args, ner_data_loader)
+    elif args.model_arc == "sep_cnn_only":
+	print "Using seperate encoders for embedding and features (cnn char)!"
+	model = Sep_CNN_Encoder_CRF_model(args, ner_data_loader)
+    else:
+	raise NotImplementedError
+    
+    model.load()
+    acc, precision, recall, f1 = evaluate(ner_data_loader, path, model, model_name)
+    return acc, precision, recall, f1
+
 
 def test_on_full_setE(ner_data_loader, args, data):
 
@@ -297,7 +321,7 @@ def main(args):
                 print("Epoch = %d, Updates = %d, CRF Loss=%f, Accumulative Loss=%f." % (epoch, updates, loss_val, cum_loss*1.0/tot_example))
             if updates % valid_freq == 0:
                 if not args.isLr:
-                    acc, precision, recall, f1 = evaluate(ner_data_loader, args.test_path, model, args.model_name)
+                    acc, precision, recall, f1 = evaluate(ner_data_loader, args.dev_path, model, args.model_name)
                 else:
                     if args.valid_on_full:
                         acc, precision, recall, f1 = evaluate_lr(ner_data_loader, model, args.model_name, args.score_file, args.setEconll, data_valid)
@@ -332,23 +356,32 @@ def main(args):
                 # if decay_num > decay_patience:
                     print("Early stop!")
                     print("Best on validation: acc=%f, prec=%f, recall=%f, f1=%f" % tuple(best_results))
-                    #Test on full SetE
-                    acc, precision, recall, f1 = test_on_full_setE(ner_data_loader, args, data_test)
-                    results = [acc, precision, recall, f1]
-                    print("Test Result: acc=%f, prec=%f, recall=%f, f1=%f" % tuple(results))
-
-                    # post processing
-                    post_process(args, best_output_fname)
+		    if args.isLr:
+			#Test on full SetE
+			acc, precision, recall, f1 = test_on_full_setE(ner_data_loader, args, data_test)
+			results = [acc, precision, recall, f1]
+			print("Test Result: acc=%f, prec=%f, recall=%f, f1=%f" % tuple(results))
+			# post processing
+			post_process(args, best_output_fname)
+		    else:
+			acc,precision,recall, f1 = evaluate_test(ner_data_loader, args.test_path, args.model_name)
+			results = [acc, precision, recall, f1]
+			print("Test Result: acc=%f, prec=%f, recall=%f, f1=%f" % tuple(results))
                     exit(0)
                 valid_history.append(f1)
         epoch += 1
 
-     # Test on full SetE
-    acc, precision, recall, f1 = test_on_full_setE(ner_data_loader, args, data_test)
-    results = [acc, precision, recall, f1]
-    print("Test Result: acc=%f, prec=%f, recall=%f, f1=%f" % tuple(results))
-    # post processing
-    post_process(args, best_output_fname)
+    if args.isLr:
+	# Test on full SetE
+	acc, precision, recall, f1 = test_on_full_setE(ner_data_loader, args, data_test)
+	results = [acc, precision, recall, f1]
+	print("Test Result: acc=%f, prec=%f, recall=%f, f1=%f" % tuple(results))
+	# post processing
+	post_process(args, best_output_fname)
+    else:
+	acc,precision,recall, f1 = evaluate_test(ner_data_loader, args.test_path, args.model_name)
+	results = [acc, precision, recall, f1]
+	print("Test Result: acc=%f, prec=%f, recall=%f, f1=%f" % tuple(results))
 
     print("All Epochs done.")
     #print("Best on validation: acc=%f, prec=%f, recall=%f, f1=%f" % tuple(best_results))
