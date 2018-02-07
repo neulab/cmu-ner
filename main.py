c90622edb3e698735feab981230a50eebb67678e
Aditi <aditiasac@gmail.com>
2018-02-07 08:41:19 -0500

    ,

--- main.py
+++ main.py
@@ -836,6 +836,8 @@ if __name__ == "__main__":
     # args = init_config()
     if args.mode == "train":
         args.load_from_path = args.save_to_path
+	print "partial_crf"
+	exit(0)
         main(args)
     elif args.mode == "test_1":
         test_single_model(args)
