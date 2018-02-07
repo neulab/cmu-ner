41db4c853dd145fe4a6eeaed65962cc5c887cd14
Aditi <aditiasac@gmail.com>
2018-02-07 08:39:51 -0500

    .

--- main.py
+++ main.py
@@ -853,6 +853,8 @@ if __name__ == "__main__":
     # args = init_config()
     if args.mode == "train":
         args.load_from_path = args.save_to_path
+	print "in evalEng"
+	exit(0)
         main(args)
     elif args.mode == "test_1":
         if args.isLr:
